42bfeff4e764da2423df5bd76f0232449cce2853
benncame29 <cbenner@flemingcollege.ca>
2024-05-31 12:05:50 -0400

    Revert "N?A"
    This reverts commit 28b13a7d5e75cbf2985d48134e27c31574f8e8a9.

--- Script2.py
+++ Script2.py
@@ -1,3 +1,2 @@
 print("Flat is better than nested.")
 print("In the face of ambiguity, refuse the temptation to guess.")
-#Zen of python
--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
 print("Now is better than never.")
+print("Although never is often better than *right* now.")
 #Zen of python
